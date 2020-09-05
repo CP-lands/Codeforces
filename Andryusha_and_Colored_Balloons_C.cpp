37214b816a57de6be8502eb486b447ec982575e1
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 04:48:14 +0900

    Update Andryusha_and_Colored_Balloons_C.cpp

--- Andryusha_and_Colored_Balloons_C.cpp
+++ Andryusha_and_Colored_Balloons_C.cpp
@@ -12,49 +12,6 @@ using namespace std;
 typedef long long ll; 
 const int MOD = 1e9 + 7;
 const int MAXN = 1e5 + 100;
- 
-bool isPrime(ll n)
-{
-	if(n < 2) return false;
-	if(n == 2) return true;
-	for(ll i = 2; i * i <= n; i++)
-		if(n % i == 0) return false;
-	return true;
-}
-ll power(ll a, ll k)
-{
-	if (k == 0) return 1;
-	if (k == 1) return a;
-	ll tmp = power(a, k >> 1);
-	tmp = (tmp * tmp) % MOD;
-	return k % 2 ? (tmp * a) % MOD : tmp;
-}
-string int_to_string(int n)
-{
-	string res = "";
-	while(n / 10 > 0)
-	{
-		res += n % 10 + '0';
-		n /= 10;
-	}
-	res += n % 10 + '0';
-	string ans = "";
-	for(int i = res.size() - 1; i >= 0; i--)
-		ans += res[i];
-	return ans;
-}
-ll string_to_int(string s)
-{
-	ll res = 0;
-	for(int i = 0; i < s.size(); i++)
-		res += (s[i] - '0') * power(10, s.size() - 1- i);
-	return res;
-}
-int get_gcd(int a, int b)
-{
-	if(b == 0) return a;
-	return get_gcd(b, a % b);
-}
 
 void Anivia_kid(){
 	int n;
