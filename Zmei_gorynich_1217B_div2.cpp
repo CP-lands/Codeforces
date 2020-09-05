1c96aea40c9d262a8335675fc4b8cbe84ab9d8b5
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 05:33:45 +0900

    Update Zmei_gorynich_1217B_div2.cpp

--- Zmei_gorynich_1217B_div2.cpp
+++ Zmei_gorynich_1217B_div2.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 
+//https://codeforces.com/problemset/problem/1217/B
 using namespace std;
 
-int solution(int x, int d, int h, int max)
-{
+int solution(int x, int d, int h, int max) {
 	if(d < x && d <= h)
 		return -1;
 	if(d >= x)
 		return 1;
-	else
-	{
+	else {
 		if((x - max) % (d - h) == 0)
 			return (x - max)/(d - h) + 1;
 		else
@@ -17,25 +16,21 @@ int solution(int x, int d, int h, int max)
 	}
 }
 
-int main()
-{
+int main() {
 	int t;
 	cin>>t;
-	for(int i = 0; i < t; i++)
-	{
+	for(int i = 0; i < t; i++) {
 		int n, x;
 		int count = INT_MAX;
 		cin>>n>>x;
 		int a[2*n];
 		int max = 0;
-		for(int i = 0; i < 2*n; i = i + 2)
-		{
+		for(int i = 0; i < 2*n; i = i + 2) {
 			cin>>a[i]>>a[i+1];
 			if(a[i] > max)
 				max = a[i];
 		}
-		for(int i = 0; i < 2*n; i = i + 2)
-		{
+		for(int i = 0; i < 2*n; i = i + 2) {
 			int temp = solution(x, a[i], a[i + 1], max);
 			if(temp < count && temp > 0)
 				count = temp;
