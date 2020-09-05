de2fe38e65d6a2bfcc75bfb4168169534403262b
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 07:51:36 +0900

    Update text_editor_C_div2.cpp

--- text_editor_C_div2.cpp
+++ text_editor_C_div2.cpp
@@ -1,55 +1,13 @@
 //------Anivia_kid----------
 #include<bits/stdc++.h>
  
+//https://codeforces.com/problemset/problem/253/C
 using namespace std;
  
 #define fi first
 #define se second
 typedef long long ll; 
 const int MOD = 1e9 + 7;
- 
-bool isPrime(ll n)
-{
-	if(n < 2) return false;
-	if(n == 2) return true;
-	for(ll i = 2; i * i <= n; i++)
-		if(n % i == 0) return false;
-	return true;
-}
-ll power(ll a, ll k)
-{
-	if (k == 0) return 1;
-	if (k == 1) return a;
-	ll tmp = power(a, k >> 1);
-	tmp = (tmp * tmp) % MOD;
-	return k % 2 ? (tmp * a) % MOD : tmp;
-}
-string int_to_string(int n)
-{
-	string res = "";
-	while(n / 10 > 0)
-	{
-		res += n % 10 + '0';
-		n /= 10;
-	}
-	res += n % 10 + '0';
-	string ans = "";
-	for(int i = res.size() - 1; i >= 0; i--)
-		ans += res[i];
-	return ans;
-}
-ll string_to_int(string s)
-{
-	ll res = 0;
-	for(int i = 0; i < s.size(); i++)
-		res += (s[i] - '0') * power(10, s.size() - 1- i);
-	return res;
-}
-int get_gcd(int a, int b)
-{
-	if(b == 0) return a;
-	return get_gcd(b, a % b);
-}
 
 void Anivia_kid(){
 	int n;
