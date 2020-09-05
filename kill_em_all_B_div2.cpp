033b98a0efbc6cfab2605c9592583883fa702151
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 07:01:08 +0900

    Update kill_em_all_B_div2.cpp

--- kill_em_all_B_div2.cpp
+++ kill_em_all_B_div2.cpp
@@ -1,24 +1,21 @@
 #include<bits/stdc++.h>
  
+//https://codeforces.com/problemset/problem/1238/B
 using namespace std;
  
-int main()
-{
+int main() {
 	int q;
 	cin>>q;
-	for(int i = 0; i < q; i++)
-	{
+	for(int i = 0; i < q; i++) {
 		int n, r, tmp;
 		cin>>n>>r;
 		set<int> res;
 		int count = 0;
-		for(int j = 0; j < n; j++)
-		{
+		for(int j = 0; j < n; j++) {
 			cin>>tmp;
 			res.insert(tmp);
 		}
-		while(!res.empty())
-		{
+		while(!res.empty()) {
 			count++;
 			tmp = *res.rbegin();
 			res.erase(tmp);
