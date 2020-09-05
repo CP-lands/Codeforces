078bfeda14583fd22eb1169e8b01d7e9410399e4
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 07:57:13 +0900

    Update the_world_is_a_theatre_C_div2.cpp

--- the_world_is_a_theatre_C_div2.cpp
+++ the_world_is_a_theatre_C_div2.cpp
@@ -1,22 +1,19 @@
 #include<bits/stdc++.h>
  
+//https://codeforces.com/problemset/problem/131/C
 using namespace std;
  
-long long comb(int n, int k)
-{
+long long comb(int n, int k) {
 	if(n == k)
 		return 1;
 	return (n * comb(n - 1, k)) / (n - k);
 }
  
-int main()
-{
- 
+int main() {
 	int n, m , t;
 	cin>>n>>m>>t;
 	long long res = 0;
-	for(int i = 4; i <= n; i++)
-	{
+	for(int i = 4; i <= n; i++) {
 		if(t - i > m || t - i < 1)
 			continue;
 		res +=  comb(n, i)*comb(m, t - i);
