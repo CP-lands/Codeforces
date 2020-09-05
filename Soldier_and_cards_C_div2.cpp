eedf180bb0da24b72e025dd395df195a4a6f41e1
foundnt_Alice <45430821+foundntAlice@users.noreply.github.com>
2020-09-06 05:28:44 +0900

    Update Soldier_and_cards_C_div2.cpp

--- Soldier_and_cards_C_div2.cpp
+++ Soldier_and_cards_C_div2.cpp
@@ -3,29 +3,24 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
 	int n,k1,k2,tmp;
 	int count = 0;
 	cin>>n;
 	queue<int> a,b;
 	cin>>k1;
-	for(int i = 0; i < k1; i++)
-	{
+	for(int i = 0; i < k1; i++){
 		cin>>tmp;
 		a.push(tmp);
 	}
 	cin>>k2;
-	for(int i = 0; i < k2; i++)
-	{
+	for(int i = 0; i < k2; i++){
 		cin>>tmp;
 		b.push(tmp);
 	}
-	while(!a.empty() && !b.empty())
-	{
+	while(!a.empty() && !b.empty()){
 		count++;
-		if(count == 1000)
-		{
+		if(count == 1000){
 			cout<<-1<<endl;
 			return 0;
 		}
@@ -33,13 +28,11 @@ int main()
 		a.pop();
 		int b_temp = b.front();
 		b.pop();
-		if(a_temp > b_temp)
-		{
+		if(a_temp > b_temp){
 			a.push(b_temp);
 			a.push(a_temp);
 		}
-		else
-		{
+		else{
 			b.push(a_temp);
 			b.push(b_temp);
 		}
